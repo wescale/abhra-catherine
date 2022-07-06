6814a31825af81145228472898a593d11925283e
Pablo Lopez <plopez@xebia.fr>
2022-07-06 12:09:17 +0200

    Fix ns

--- commands.sh
+++ commands.sh
@@ -1,4 +1,7 @@
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm dep update charts/argo-cd/
 
-helm install -n argo-cd --create-namespace argo-cd charts/argo-cd/
+helm install -n tools-argo-cd --create-namespace argo-cd charts/argo-cd/
+
+kubectl -n tools-argo-cd apply -f configuration/1-argo-cd-project.yaml
+kubectl -n tools-argo-cd apply -f configuration/2-argo-cd-application.yaml
