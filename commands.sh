ffd889b1c66eac65ecb8c1dc02b12d534ef9950f
Pablo Lopez <plopez@xebia.fr>
2022-07-06 14:11:36 +0200

    cert-manager

--- commands.sh
+++ commands.sh
@@ -3,5 +3,5 @@ helm dep update charts/argo-cd/
 
 helm install -n tools-argo-cd --create-namespace argo-cd charts/argo-cd/
 
-kubectl -n tools-argo-cd apply -f configuration/1-argo-cd-project.yaml
-kubectl -n tools-argo-cd apply -f configuration/2-argo-cd-application.yaml
+kubectl -n tools-argo-cd apply -f configuration/argo-cd-project.yaml
+kubectl -n tools-argo-cd apply -f configuration/argo-cd-application.yaml
