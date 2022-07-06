b90f125df54fb22f1900bd069fdaaf5e480f1996
Pablo Lopez <plopez@xebia.fr>
2022-07-06 17:20:00 +0200

    Fucking crds

--- commands.sh
+++ commands.sh
@@ -1,7 +1,18 @@
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm dep update charts/argo-cd/
 
-helm install -n tools-argo-cd --create-namespace argo-cd charts/argo-cd/
+helm install -n tools-argo-cd --create-namespace abhra charts/argo-cd/
+
+kubectl create namespace tools-sealed-secrets
+
+kubectl --namespace tools-sealed-secrets apply -f secret/master.key
 
 kubectl -n tools-argo-cd apply -f configuration/argo-cd-project.yaml
 kubectl -n tools-argo-cd apply -f configuration/argo-cd-application.yaml
+
+# kubectl get secret -n tools-sealed-secrets -l sealedsecrets.bitnami.com/sealed-secrets-key -o yaml > secrets/master.key
+# kubectl get secret -n tools-sealed-secrets -o yaml >> secrets/master.key
+
+
+
+
