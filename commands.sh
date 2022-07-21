443d0902e65c4d49bc9bc0b158a295aa50beb909
Pablo Lopez <plopez@xebia.fr>
2022-07-21 17:19:26 +0200

    Argo self managed

--- commands.sh
+++ commands.sh
@@ -1,11 +1,12 @@
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm dep update charts/argo-cd/
 
-helm install -n tools-argo --create-namespace abhra charts/argo-cd/
-
 kubectl create namespace tools-sealed-secrets
 
 kubectl --namespace tools-sealed-secrets apply -f secrets/master.key
+
+helm install -n tools-argo --create-namespace abhra argo-cd/
+
 
 kubectl -n tools-argo apply -f configuration/argo-cd-project.yaml
 kubectl -n tools-argo apply -f configuration/argo-cd-application.yaml
