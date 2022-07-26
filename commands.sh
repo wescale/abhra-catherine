acd431f13754607776dea1b8f30f132069788110
Pablo Lopez <plopez@xebia.fr>
2022-07-26 13:46:48 +0200

    Drone

--- commands.sh
+++ commands.sh
@@ -15,5 +15,13 @@ kubectl -n tools-argo apply -f configuration/argo-cd-application.yaml
 # kubectl get secret -n tools-sealed-secrets -o yaml >> secrets/master.key
 
 
+kubectl create namespace tools-sealed-secrets
+kubectl --namespace tools-sealed-secrets apply -f secrets/master.key
+helm install -n tools-sealed-secrets  abhra helm/sealed-secrets
+kubectl create namespace tools-argo
+kubectl --namespace tools-argo apply -f sealed-secrets/google-secret.yaml
+helm install -n tools-argo  abhra helm/argo-cd (modifié) 
+
+
 
 
